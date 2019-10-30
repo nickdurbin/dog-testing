be86f4f44ba922548a49ebb97ffa814c63070cb6
nick.durbin <nick.durbin@gmail.com>
2019-10-29 20:58:58 -0400

    Added some jest functions with mocks to chekc out async tests.

--- src/App.test.js
+++ src/App.test.js
@@ -4,14 +4,26 @@ import * as rtl from "@testing-library/react";
 // not importing to a variable, since this just overrides jest global variables
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
+import { JestEnvironment } from "@jest/environment";
+
+jest.mock('axios', () => {
+  return {
+    get: jest.fn(() => Promise.resolve({
+      data: {
+        message: ['foo.jpg', 'bar.jpg']
+      }
+    }))
+  }
+})
 
 // this just allows react-testing-library to do some
 // routine cleanup after each test runs (to reset the DOM and such)
 afterEach(rtl.cleanup);
 
-test("Render the heading", () => {
+test("Render the heading", async () => {
   // render our React app into an in-memory DOM so we can test against it
   const wrapper = rtl.render(<App />);
+  await wrapper.findAllByAltText(/dog/i);
 
   // element is now our dom element that we can test against
   const element = wrapper.getByText(/the dog website/i);
@@ -29,8 +41,8 @@ test("Render count input", () => {
   expect(element).toHaveValue(1);
 });
 
-test("<App /> snapshot", () => {
+test("<App /> snapshot", async () => {
   const wrapper = rtl.render(<App />)
-
+  await wrapper.findAllByAltText(/dog/i);
   expect(wrapper.asFragment()).toMatchSnapshot()
 })
--- src/utils/wait.test.js
+++ src/utils/wait.test.js
@@ -1,22 +1,25 @@
 import wait from './wait';
 
 // Method one for async tests
-test("wait for promise to resolve", (done) => {
-  wait(3).then(result => {
-    expect(result).toBe("hurray")
-    done()
-  })
-})
+// test("wait for promise to resolve", (done) => {
+//   wait(1).then(result => {
+//     expect(result).toBe("hurray")
+//     done()
+//   })
+//   .catch(err => {
+//     done(err)
+//   })
+// })
 
 // Method two for async tests
-test("wait for promise to resolve", () => {
-  return wait(3).then(result => {
-    expect(result).toBe("hurray")
-  })
-})
+// test("wait for promise to resolve", () => {
+//   return wait(1).then(result => {
+//     expect(result).toBe("hurray")
+//   })
+// })
 
 // Method three for async tests
 test("wait for promise to resolve", async () => {
-  const result = await wait(3)
+  const result = await wait(1)
     expect(result).toBe("hurray")
 })
